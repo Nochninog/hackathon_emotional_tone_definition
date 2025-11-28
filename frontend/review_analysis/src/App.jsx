9c691b95cd18cdc9d150cc2c3c4d9b0fed4e3eec
Nochninog <vorotnikova.elizaveta@internet.ru>
2025-11-29 02:49:21 +0300

    feat: add basic structure of main page

--- frontend/review_analysis/src/App.jsx
+++ frontend/review_analysis/src/App.jsx
@@ -1,12 +1,23 @@
 import { useState } from 'react'
-
+import styles from "./assets/App.module.scss";
+import './assets/global.scss'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <main>
+        <div className={styles['header-container']}>
+          <h1>АНАЛИЗАТОР ТОНАЛЬНОСТИ ОТЗЫВОВ</h1>
+        </div>
+        <div className={styles['upload-status-container']}>
+          <div className={styles['upload-container']}>
+              <input type="Введите название файла" />
+              <button>Загрузить данные</button>
+          </div>
+          <div className={styles['status-panel-container']}>
+
+          </div>
+        </div>
         
       </main>
      
