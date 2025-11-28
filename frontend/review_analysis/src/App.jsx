e8e986346385fbe400861c9f848e87df05be8eef
Nochninog <vorotnikova.elizaveta@internet.ru>
2025-11-28 22:39:29 +0300

    build: create vite app

---
+++ frontend/review_analysis/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <main>
+        
+      </main>
+     
+    </>
+  )
+}
+
+export default App
