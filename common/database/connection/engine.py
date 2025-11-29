ddd5a9f8f21a56bb72fb9272212ac424f7b9b970
Sanch0pansa <god@lifegame.su>
2025-11-29 21:19:41 +0300

    refactor: remove singletone session maker

--- common/database/connection/engine.py
+++ common/database/connection/engine.py
@@ -5,33 +5,26 @@ from sqlalchemy.ext.asyncio import (
     create_async_engine,
 )
 
-from .sessions import SessionMaker
-
 
 def create_engine(
-    db_name: str,
-    db_user: str,
-    db_password: str,
-    db_host: str,
-    db_port: int,
-    db_engine: str,
+    db_url: str,
 ) -> AsyncEngine:
-    db_url = f"{db_engine}://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
-    engine = create_async_engine(
+    return create_async_engine(
         url=db_url,
         echo=False,
         pool_pre_ping=True,
     )
 
-    SessionMaker.set_session_maker(
-        async_sessionmaker(
-            bind=engine,
-            class_=AsyncSession,
-            expire_on_commit=False,
-        ),
+
+def create_session_maker(
+    engine: AsyncEngine,
+) -> async_sessionmaker[AsyncSession]:
+    return async_sessionmaker(
+        bind=engine,
+        class_=AsyncSession,
+        expire_on_commit=False,
     )
 
-    return engine
 
 
 async def dispose_engine(engine: AsyncEngine) -> None:
